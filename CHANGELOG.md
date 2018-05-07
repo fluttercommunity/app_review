866b5801e4e3e57b44abb7f8a41099246570f79a
Rody Davis <rody.davis.jr@gmail.com>
2018-05-07 08:10:39 -0400

    Published to Dart Pub

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## [0.0.1] - TODO: Add release date.
 
-* TODO: Describe initial release.
+* Created Tool to Request and Write Reviews on iOS and Navigate to the Store Listing on both Android and iOS. There is also the ability to access the App ID from both Android and iOS which can be used for Admob, Firebase and other tools. Apps that are not published yet will say "Item Not Found" when it navigates to the Store. The App ID must match the App ID in iTunes Connect and Google Play.
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,11 +6,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:app_review_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
+  testWidgets('Verify App ID', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
@@ -18,7 +17,7 @@ void main() {
     expect(
         find.byWidgetPredicate(
           (Widget widget) =>
-              widget is Text && widget.data.startsWith('Running on:'),
+              widget is Text && widget.data.startsWith('App ID'),
         ),
         findsOneWidget);
   });
