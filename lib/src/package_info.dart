90c32a1023bb8b3ef9f7740fef5d3746e18c9d8f
shinsenter <2082119+shinsenter@users.noreply.github.com>
2020-08-07 11:42:13 +0900

    Minimize code logic for v2.0

--- lib/src/package_info.dart
+++ lib/src/package_info.dart
@@ -1,25 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 
-Future<PackageInfo> getPackageInfo() async {
-  final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+import '../app_review.dart';
 
-  final String appName = packageInfo.appName;
-  final String packageName = packageInfo.packageName;
-  final String version = packageInfo.version;
-  final String buildNumber = packageInfo.buildNumber;
+/// Lazyload package info instance
+Future<PackageInfo> getPackageInfo() async => AppReview.getPackageInfo();
 
-  debugPrint("""
-App Name: $appName
-Package Name: $packageName
-Version: $version
-Build Number: $buildNumber
-""");
-
-  return packageInfo;
-}
-
-Future<String> getPackageName() async {
-  final info = await getPackageInfo();
-  return info.packageName;
-}
+/// Get package bundle name
+Future<String> getPackageName() async => AppReview.getBundleName();
